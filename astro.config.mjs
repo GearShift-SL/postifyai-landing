077eaf310cfe03de7d57bafaad2d4ce0a4f2944c
Daniel Garcia <github@mail.daniel.es>
2025-10-01 17:13:26 +0000

    Update Astro configuration: standardize quotation marks for consistency, add server host configuration, and refine sitemap filter logic.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true, // or '0.0.0.0'
+    // port: 4321,     // optional
+  },
   vite: {
     plugins: [tailwindcss()],
   },
 
-  site: "https://miseo.ai",
+  site: 'https://miseo.ai',
   integrations: [
     icon(),
     sitemap({
-      filter: (page) => page !== "https://miseo.ai/stripe-callback/",
+      filter: (page) => page !== 'https://miseo.ai/stripe-callback/',
     }),
     mdx(),
     react(),
   ],
-  trailingSlash: "always",
+  trailingSlash: 'always',
 });
